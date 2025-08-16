bea135154bd911ca1823e0141a2dcdc7f2bbf101
shriguruu <gshriguruu@gmail.com>
2025-08-16 20:50:20 +0530

    Update data

--- Lab4/product-app/src/data/products.js
+++ Lab4/product-app/src/data/products.js
@@ -1,3 +1,4 @@
+// Data for the Product App
 export const PRODUCTS = [
   { category: 'Fruits', price: '$1', stocked: true, name: 'Apple' },
   { category: 'Fruits', price: '$1', stocked: true, name: 'Dragonfruit' },
