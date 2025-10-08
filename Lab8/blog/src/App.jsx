f0cb801c645dfec0b50ee348f267eaec4331d324
shriguruu <gshriguruu@gmail.com>
2025-10-08 22:21:19 +0530

    Update App.jsx

--- Lab8/blog/src/App.jsx
+++ Lab8/blog/src/App.jsx
@@ -4,26 +4,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import BlogNavbar from './components/BlogNavBar.jsx';
-import PostList from './components/PostList';
-import blogPosts from './components/data.js';
+import HomePage from './pages/Home.jsx';
+import PostsPage from './pages/Posts.jsx';
 import './App.css';
 
 function App() {
-   const PostsPage = () => (
-    <>
-      <h1 className="mb-4">All Blog Posts</h1>
-      <PostList posts={blogPosts} />
-    </>
-  );
-
-  // A simple component for the home page message
-  const HomePage = () => (
-    <>
-      <h1 className="mb-4">Welcome Home!</h1>
-      <p>Here are our most recent posts.</p>
-      <PostList posts={blogPosts} />
-    </>
-  );
   return (
     <div className="App">
       <BrowserRouter>
